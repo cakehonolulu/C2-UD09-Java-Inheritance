074a9c9c039bceec3978632c03419aacdde92846
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-14 10:15:55 +0200

    Task1: Add a simple main method that prints the newly created object to verify the logic behind the getters/setters/submethods

---
+++ src/Task1/Task1App.java
@@ -0,0 +1,13 @@
+package Task1;
+
+public class Task1App {
+
+	public static void main(String[] args) {
+		Electrodomestico prueba = new Electrodomestico(60, "blanco", 'A', 30);
+		
+		System.out.println(prueba);
+		
+
+	}
+
+}
