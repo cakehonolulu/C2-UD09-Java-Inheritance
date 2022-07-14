a14bb7f95f1a688012f3d2fe606abde2638b68db
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-14 10:38:32 +0200

    Task1: Implement precioFinal()

--- src/Task1/Electrodomestico.java
+++ src/Task1/Electrodomestico.java
@@ -1,113 +1,157 @@
 package Task1;
 
 public class Electrodomestico {
-		public String colores[] = new String[] {"blanco", "negro", "rojo", "azul", "gris"};
-		public char consumos[] = new char[] {'A', 'B', 'C', 'D', 'E', 'F'};
-		
-		protected double precioBase;
-		protected String color;
-		protected char consumoEnerg;
-		protected double peso;
+	public String colores[] = new String[] { "blanco", "negro", "rojo", "azul", "gris" };
+	public char consumos[] = new char[] { 'A', 'B', 'C', 'D', 'E', 'F' };
 
-	    private final String COLOR = colores[0];
-	    private final char CONSUMO = consumos[1];
-	    private final double PRECIO_BASE = 100;
-	    private final double PESO = 5;
+	protected double precioBase;
+	protected String color;
+	protected char consumoEnerg;
+	protected double peso;
 
-	    public Electrodomestico() {
-	        this.precioBase = PRECIO_BASE;
-	        this.color = COLOR;
-	        this.consumoEnerg = CONSUMO;
-	        this.peso = PESO;
-	    }
+	private final String COLOR = colores[0];
+	private final char CONSUMO = consumos[1];
+	private final double PRECIO_BASE = 100;
+	private final double PESO = 5;
 
-	    public Electrodomestico(double precioBase, double peso) {
-	        this.precioBase = precioBase;
-	        this.peso = peso;
-	        this.color = COLOR;
-	        this.consumoEnerg = CONSUMO;
-	    }
+	public Electrodomestico() {
+		this.precioBase = PRECIO_BASE;
+		this.color = COLOR;
+		this.consumoEnerg = CONSUMO;
+		this.peso = PESO;
+	}
 
-	    public Electrodomestico(double precioBase, String color, char consumoEnerg, double peso) {
-	    	consumoEnerg = Character.toUpperCase(consumoEnerg);
-	    	consumoEnerg = comprobarConsumoEnergetico(consumoEnerg);
-	    	color = color.toLowerCase();
-	    	color = comprobarColor(color);
-	        this.precioBase = precioBase;
-	        this.color = color;
-	        this.consumoEnerg = consumoEnerg;
-	        this.peso = peso;
-	    }
+	public Electrodomestico(double precioBase, double peso) {
+		this.precioBase = precioBase;
+		this.peso = peso;
+		this.color = COLOR;
+		this.consumoEnerg = CONSUMO;
+		precioFinal();
+	}
 
-	    public double getPrecioBase() {
-	        return precioBase;
-	    }
+	public Electrodomestico(double precioBase, String color, char consumoEnerg, double peso) {
+		consumoEnerg = Character.toUpperCase(consumoEnerg);
+		consumoEnerg = comprobarConsumoEnergetico(consumoEnerg);
+		color = color.toLowerCase();
+		color = comprobarColor(color);
+		this.precioBase = precioBase;
+		this.color = color;
+		this.consumoEnerg = consumoEnerg;
+		this.peso = peso;
+		precioFinal();
+	}
 
-	    public void setPrecioBase(double precioBase) {
-	        this.precioBase = precioBase;
-	    }
+	public double getPrecioBase() {
+		return precioBase;
+	}
 
-	    public String getColor() {
-	        return color;
-	    }
+	public void setPrecioBase(double precioBase) {
+		this.precioBase = precioBase;
+	}
 
-	    public void setColor(String color) {
-	        this.color = color;
-	    }
+	public String getColor() {
+		return color;
+	}
 
-	    public char getConsumoEnerg() {
-	        return consumoEnerg;
-	    }
+	public void setColor(String color) {
+		this.color = color;
+	}
 
-	    public void setConsumoEnerg(char consumoEnerg) {
-	        this.consumoEnerg = consumoEnerg;
-	    }
+	public char getConsumoEnerg() {
+		return consumoEnerg;
+	}
 
-	    public double getPeso() {
-	        return peso;
-	    }
+	public void setConsumoEnerg(char consumoEnerg) {
+		this.consumoEnerg = consumoEnerg;
+	}
 
-	    public void setPeso(double peso) {
-	        this.peso = peso;
-	    }
+	public double getPeso() {
+		return peso;
+	}
 
-	    public char comprobarConsumoEnergetico(char letra) {
-	    	boolean m_valid = false;
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
 
-	    	for (int i = 0; i < consumos.length; i++) {
-	    		if (letra == consumos[i]) {
-	    			m_valid = true;
-				}	
+	public char comprobarConsumoEnergetico(char letra) {
+		boolean m_valid = false;
+
+		for (int i = 0; i < consumos.length; i++) {
+			if (letra == consumos[i]) {
+				m_valid = true;
 			}
-	    	
-	    	if (!m_valid) {
-	    		letra = CONSUMO;
-	    	}
-	    	
-	    	return letra;
-	    	
-	    }
+		}
 
-	    public String comprobarColor(String color) {
-	    	boolean m_valid = false;
+		if (!m_valid) {
+			letra = CONSUMO;
+		}
 
-	    	for (int i = 0; i < colores.length; i++) {
-	    		if (color.equals(colores[i])) {
-	    			m_valid = true;
-				}	
+		return letra;
+
+	}
+
+	public String comprobarColor(String color) {
+		boolean m_valid = false;
+
+		for (int i = 0; i < colores.length; i++) {
+			if (color.equals(colores[i])) {
+				m_valid = true;
 			}
-	    	
-	    	if (!m_valid) {
-	    		color = COLOR;
-	    	}
-	    	
-	    	return color;
-	    	
-	    }
-	    
-	    @Override
-	    public String toString() {
-	        return "Electrodomestico [precioBase=" + precioBase + ", color=" + color + ", consumoEnerg=" + consumoEnerg
-	                + ", peso=" + peso + "]";
-	    }
+		}
+
+		if (!m_valid) {
+			color = COLOR;
+		}
+
+		return color;
+
+	}
+
+	public void precioFinal() {
+		double precioFinal = this.precioBase;
+
+		switch (this.consumoEnerg) {
+			case 'A':
+				precioFinal += 100;
+				break;
+	
+			case 'B':
+				precioFinal += 80;
+				break;
+	
+			case 'C':
+				precioFinal += 60;
+				break;
+	
+			case 'D':
+				precioFinal += 50;
+				break;
+	
+			case 'E':
+				precioFinal += 30;
+				break;
+	
+			case 'F':
+				precioFinal += 10;
+				break;
+		}
+
+		if (this.peso >= 0 && this.peso <= 19) {
+			precioFinal += 10;
+		} else if (this.peso >= 20 && this.peso <= 49) {
+			precioFinal += 50;
+		} else if (this.peso >= 20 && this.peso <= 49) {
+			precioFinal += 80;
+		} else {
+			precioFinal += 100;
+		}
+
+		this.precioBase = precioFinal;
+	}
+
+	@Override
+	public String toString() {
+		return "Electrodomestico [precioBase=" + precioBase + ", color=" + color + ", consumoEnerg=" + consumoEnerg
+				+ ", peso=" + peso + "]";
+	}
 }
