11d62cb13308f63bb9f2671f65456469d4517638
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-14 12:53:56 +0200

    Task2: Add array of 5 'Juegos' and fill each

--- src/Task2/Task2App.java
+++ src/Task2/Task2App.java
@@ -3,8 +3,14 @@ package Task2;
 public class Task2App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Videojuego[] videojuegos = new Videojuego[5];
+		
+		videojuegos[0] = new Videojuego("Resident Evil: Dead Aim", 67);
+		videojuegos[1] = new Videojuego("Animal Crossing", 123, "Sandbox", "Nintendo");
+		videojuegos[2] = new Videojuego("Project Zomboid", 20);
+		videojuegos[3] = new Videojuego("Monster Hunter", 12);
+		videojuegos[4] = new Videojuego("inFamous", 50, "Sandbox", "Sucker Punch");
 	}
 
 }
