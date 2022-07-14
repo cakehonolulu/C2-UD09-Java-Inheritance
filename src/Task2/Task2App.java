6400a836c4f224d3aa3bc87dd79d4e773c767760
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-14 12:55:25 +0200

    created Serie array

--- src/Task2/Task2App.java
+++ src/Task2/Task2App.java
@@ -3,8 +3,11 @@ package Task2;
 public class Task2App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Serie series[] = { new Serie("harry potter", 2, "comedia", "pepito"), 
+				new Serie("love", 4, "drama", "juanito"),
+				new Serie("risas y mas risas", 5, "drama", "antonieta"),
+				new Serie("con faldas y a lo loco", 6, "historia", "cleopatra"),
+				new Serie("animales en la selva", 2, "documental", "fito") };
 	}
 
 }
