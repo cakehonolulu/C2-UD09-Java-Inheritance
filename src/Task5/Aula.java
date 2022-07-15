401d372719c6abea57ed5e1c8bd251ead89ddd05
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-15 09:38:34 +0200

    added enum Materia in Aula's atributs

--- src/Task5/Aula.java
+++ src/Task5/Aula.java
@@ -3,16 +3,18 @@ package Task5;
 public class Aula {
 	int id;
 	int maxEst;
-	//enum materia;
-	
-	public Aula(int id, int maxEst) {
+	Materia materia;
+
+	public Aula(int id, int maxEst, Materia materia) {
 		this.id = id;
 		this.maxEst = maxEst;
+		this.materia = materia;
 	}
-	
+
 	public Aula() {
 		this.id = 0;
 		this.maxEst = 0;
+		this.materia = Materia.sin_materia;
 	}
 
 	public int getId() {
@@ -31,12 +33,17 @@ public class Aula {
 		this.maxEst = maxEst;
 	}
 
+	public Materia getMateria() {
+		return materia;
+	}
+
+	public void setMateria(Materia materia) {
+		this.materia = materia;
+	}
+
 	@Override
 	public String toString() {
-		return "Aula [id=" + id + ", maxEst=" + maxEst + "]";
+		return "Aula [id=" + id + ", maxEst=" + maxEst + ", materia=" + materia + "]";
 	}
-	
-	
-	
-	
+
 }
