9e968dcc28d7d685082c7967c45b5fc4238cade6
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-15 09:04:00 +0200

    Task5: Add default class constructors

--- src/Task5/Persona.java
+++ src/Task5/Persona.java
@@ -5,4 +5,18 @@ public class Persona {
 		String nombre;
 		int edad;
 		String sexo;
+		
+		public Persona() {
+			this.nombre = "";
+			this.edad = 0;
+			this.sexo = "";
+		}
+		
+		public Persona(String nombre, int edad, String sexo) {
+			this.nombre = nombre;
+			this.edad = edad;
+			this.sexo = sexo;
+		}
+		
+		
 }
