d3c829d4729876c3d4a7d386c5698c031b778f01
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-15 09:46:47 +0200

    added atributs in Aula

--- src/Task5/Aula.java
+++ src/Task5/Aula.java
@@ -1,20 +1,47 @@
 package Task5;
 
+import java.util.Arrays;
+
 public class Aula {
 	int id;
 	int maxEst;
 	Materia materia;
+	Alumno alumnos[];
+	Profesor profesor;
 
-	public Aula(int id, int maxEst, Materia materia) {
+	public Aula(int id, int maxEst, Materia materia, Profesor profesor, Alumno alumnos[]) {
 		this.id = id;
 		this.maxEst = maxEst;
 		this.materia = materia;
+		this.profesor = profesor;
+		this.alumnos = alumnos;
 	}
 
 	public Aula() {
 		this.id = 0;
 		this.maxEst = 0;
 		this.materia = Materia.sin_materia;
+		this.profesor=null;
+		this.alumnos=null;
+
+	}
+	
+	
+
+	public Alumno[] getAlumnos() {
+		return alumnos;
+	}
+
+	public Profesor getProfesor() {
+		return profesor;
+	}
+
+	public void setAlumnos(Alumno[] alumnos) {
+		this.alumnos = alumnos;
+	}
+
+	public void setProfesor(Profesor profesor) {
+		this.profesor = profesor;
 	}
 
 	public int getId() {
@@ -43,7 +70,10 @@ public class Aula {
 
 	@Override
 	public String toString() {
-		return "Aula [id=" + id + ", maxEst=" + maxEst + ", materia=" + materia + "]";
+		return "Aula [id=" + id + ", maxEst=" + maxEst + ", materia=" + materia + ", alumnos="
+				+ Arrays.toString(alumnos) + ", profesor=" + profesor + "]";
 	}
+
+	
 
 }
