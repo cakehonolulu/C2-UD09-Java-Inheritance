5d3e968d67d30d973b33019bab10a914b2c72259
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-15 08:49:03 +0200

    added atributs in Raices

--- src/Task4/Raices.java
+++ src/Task4/Raices.java
@@ -1,5 +1,22 @@
 package Task4;
 
 public class Raices {
+	private double a;
+	private double b;
+	private double c;
+	
+	
+	
+	
+	public Raices(double a, double b, double c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	
+	
+	
 
 }
