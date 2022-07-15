9f8f7d2e3fe6424b343d1756a24b5f277f12e2cf
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-15 10:18:16 +0200

    Task5: Add alumnos arrays

--- src/Task5/Task5App.java
+++ src/Task5/Task5App.java
@@ -10,7 +10,19 @@ public class Task5App {
 				new Profesor("Jose", 35, "hombre", Materia.matematicas),
 				new Profesor("Josepa", 40, "mujer", Materia.sin_materia) };
 		
+		Alumno alumnos[] = { new Alumno("Pepito1", 15, "hombre", 8),
+				new Alumno("Ana1", 16, "mujer", 5),
+				new Alumno("Nate1", 15, "n/b", 2),
+				new Alumno("Pepita1", 15, "mujer", 6),
+				new Alumno("Jose1", 16, "hombre", 7),
+				new Alumno("Josepa1", 15, "mujer", 3) };
 		
+		Alumno alumnos_2[] = { new Alumno("Pepito2", 15, "hombre", 2),
+				new Alumno("Ana2", 16, "mujer", 5),
+				new Alumno("Nate2", 15, "n/b", 6),
+				new Alumno("Pepita2", 15, "mujer", 6),
+				new Alumno("Jose2", 16, "hombre", 5),
+				new Alumno("Josepa2", 15, "mujer", 2) };
 	}
 
 }
