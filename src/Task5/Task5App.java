68748bd6c3fc3f7478a91c6088ede274a6ac5c96
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-15 10:14:59 +0200

    Task5: Add professors array

--- src/Task5/Task5App.java
+++ src/Task5/Task5App.java
@@ -3,8 +3,14 @@ package Task5;
 public class Task5App {
 
 	public static void main(String[] args) {
+		Profesor profesores[] = { new Profesor("Pepito", 40, "hombre", Materia.matematicas),
+				new Profesor("Ana", 35, "mujer", Materia.fisica),
+				new Profesor("Nate", 28, "n/b", Materia.filosofia),
+				new Profesor("Pepita", 47, "mujer", Materia.sin_materia),
+				new Profesor("Jose", 35, "hombre", Materia.matematicas),
+				new Profesor("Josepa", 40, "mujer", Materia.sin_materia) };
 		
-
+		
 	}
 
 }
