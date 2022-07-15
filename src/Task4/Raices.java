ea01bda7de8f7cfdebbfe0c576ff136087b56220
Marccudi <marccudi@hotmail.es>
2022-07-15 11:57:24 +0200

    Tiene raiz & calcular finished

--- src/Task4/Raices.java
+++ src/Task4/Raices.java
@@ -1,5 +1,7 @@
 package Task4;
 
+import java.util.ArrayList;
+
 public class Raices {
 	private double a;
 	private double b;
@@ -24,6 +26,31 @@ public class Raices {
 			return false;
 		}
 	}
+	
+	public boolean tieneRaiz() {
+		if (this.getDscriminante() == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void calcular() {
+		double suma=(-b + Math.sqrt((Math.pow(b, 2) - (4 * this.a * this.c)))/ (2/this.a));
+		double resta=(-b - Math.sqrt((Math.pow(b, 2) - (4 * this.a * this.c)))/ (2/this.a));		
+		
+			if (this.getDscriminante()<0) {//uno tiene solucion
+				System.out.println("No tiene solucion");
+			} else if(this.getDscriminante()==0 ){//una soluciones
+				System.out.println("Resultado es " + suma);
+			}else {
+				System.out.println("Resultado suma: "+ suma + "\nResultado resta: " + resta);
+			}	
+		
+	}
+	
+	
+	
 	
 	
 
