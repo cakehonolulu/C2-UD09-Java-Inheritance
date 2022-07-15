b4d336ba2b4ef29b6102f26df82c1d8a07a0ffd5
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-15 12:12:51 +0200

    Task4: Add the example in the main function that shows all the methods in use

--- src/Task4/Task4App.java
+++ src/Task4/Task4App.java
@@ -3,8 +3,16 @@ package Task4;
 public class Task4App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Raices equacion = new Raices(5, -20, 15);
 
+		if (equacion.tieneRaices()) {
+			equacion.obtenerRaices();
+		}
+		else if (equacion.tieneRaiz()) {
+			equacion.obtenerRaiz();
+		}
+		
+		equacion.calcular();
 	}
 
 }
