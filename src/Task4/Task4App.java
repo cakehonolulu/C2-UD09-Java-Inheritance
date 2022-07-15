35654a2004890cba4685b2344d720208f20b5d30
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-15 22:03:38 +0200

    created method verSolucionesEquacion() in main method task4

--- src/Task4/Task4App.java
+++ src/Task4/Task4App.java
@@ -4,14 +4,16 @@ public class Task4App {
 
 	public static void main(String[] args) {
 		Raices equacion = new Raices(5, -20, 15);
+		verSolucionesEquacion(equacion);
+	}
 
+	public static void verSolucionesEquacion(Raices equacion) {
 		if (equacion.tieneRaices()) {
 			equacion.obtenerRaices();
-		}
-		else if (equacion.tieneRaiz()) {
+		} else if (equacion.tieneRaiz()) {
 			equacion.obtenerRaiz();
 		}
-		
+
 		equacion.calcular();
 	}
 
