2dfd6659647961c2e074f4e3f530fac792b763a5
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-15 21:53:04 +0200

    separate code from main task2 into different methods and add comments

--- src/Task2/Task2App.java
+++ src/Task2/Task2App.java
@@ -4,67 +4,104 @@ public class Task2App {
 
 	public static void main(String[] args) {
 
-		Serie series[] = { new Serie("harry potter", 2, "comedia", "pepito"), new Serie("love", 4, "drama", "juanito"),
-				new Serie("risas y mas risas", 5, "drama", "antonieta"),
-				new Serie("con faldas y a lo loco", 6, "historia", "cleopatra"),
-				new Serie("animales en la selva", 2, "documental", "fito") };
+		// llena un array de Serie con objetos Serie
+		Serie series[] = arrayConSerie();
 
-		Videojuego[] videojuegos = { new Videojuego("Resident Evil: Dead Aim", 67),
-				new Videojuego("Animal Crossing", 123, "Sandbox", "Nintendo"), new Videojuego("Project Zomboid", 20),
-				new Videojuego("Monster Hunter", 12), new Videojuego("inFamous", 50, "Sandbox", "Sucker Punch") };
+		// llena un array VIdeojuego
+		Videojuego[] videojuegos = arrayConVideojuego();
 
+		// entregar series y videojuegos
 		series[0].entregar();
 		series[3].entregar();
 		videojuegos[1].entregar();
 		videojuegos[4].entregar();
 
-		
-		int contVid=0;
-		int contSer=0;
-		
-		
+		// mostrar la serie con mas temporadas y el videojuego con mas horas
+		videojuegoMasLargo(videojuegos);
+		serieConMasTemporadas(series);
+
+	}
+
+	/***
+	 * declara un array de Serie y se rellena con datos
+	 * 
+	 * @return el array con datos
+	 */
+	public static Serie[] arrayConSerie() {
+		Serie series[] = { new Serie("harry potter", 2, "comedia", "pepito"), new Serie("love", 4, "drama", "juanito"),
+				new Serie("risas y mas risas", 5, "drama", "antonieta"),
+				new Serie("con faldas y a lo loco", 6, "historia", "cleopatra"),
+				new Serie("animales en la selva", 2, "documental", "fito") };
+		return series;
+	}
+
+	/***
+	 * 
+	 * @return array con objetos Serie con valores
+	 */
+	public static Videojuego[] arrayConVideojuego() {
+		Videojuego[] videojuegos = { new Videojuego("Resident Evil: Dead Aim", 67),
+				new Videojuego("Animal Crossing", 123, "Sandbox", "Nintendo"), new Videojuego("Project Zomboid", 20),
+				new Videojuego("Monster Hunter", 12), new Videojuego("inFamous", 50, "Sandbox", "Sucker Punch") };
+		return videojuegos;
+	}
+
+	/***
+	 * imprime el videojuego con mas horas
+	 * 
+	 * @param videojuegos array con objetos Viedojuego entre los cual compara e
+	 *                    imprime el mas largo
+	 */
+	public static void videojuegoMasLargo(Videojuego videojuegos[]) {
+		int contVid = 0;
+
 		Videojuego masLargo = null;
-		
+
 		for (int i = 0; i < videojuegos.length; i++) {
 			if (videojuegos[i].isEntregado()) {
-				//System.out.println(videojuegos[i].toString());
+				// System.out.println(videojuegos[i].toString());
 				contVid++;
 			}
-			
+
 			if (masLargo == null) {
-				masLargo=videojuegos[i];
+				masLargo = videojuegos[i];
 			} else {
 				if (!videojuegos[i].compareTo(masLargo)) {
-					masLargo=videojuegos[i];
+					masLargo = videojuegos[i];
 				}
-				
-			}	
+
+			}
 		}
-		
+
 		System.out.println(masLargo.toString());
-		
-		Serie MasTemp=null;
+	}
+
+	/***
+	 * 
+	 * @param series, array con objetos tipo Serie imprime la serie con mas
+	 *                temporadas
+	 */
+	public static void serieConMasTemporadas(Serie series[]) {
+		int contSer = 0;
+
+		Serie MasTemp = null;
 		for (int i = 0; i < series.length; i++) {
 			if (series[i].isEntregado()) {
-				//System.out.println(series[i].toString());
+				// System.out.println(series[i].toString());
 				contSer++;
 			}
-			
+
 			if (MasTemp == null) {
-				MasTemp=series[i];
+				MasTemp = series[i];
 			} else {
 				if (!series[i].compareTo(MasTemp)) {
-					MasTemp=series[i];
+					MasTemp = series[i];
 				}
-				
-			}	
-			
+
+			}
+
 		}
 		System.out.println(MasTemp.toString());
-		int contTot=contVid+contSer;
-		
-		
-		
 	}
 
 }
