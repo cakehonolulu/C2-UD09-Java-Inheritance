9e78591c62098f274d2f711b904c3ed9e89c5ae4
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-18 10:54:23 +0200

    add comments in class Electrodomestico in tast1

--- src/Task1/Electrodomestico.java
+++ src/Task1/Electrodomestico.java
@@ -1,19 +1,21 @@
 package Task1;
 
 public class Electrodomestico {
+	// valores posibles para color y consumo energetico
 	public String colores[] = new String[] { "blanco", "negro", "rojo", "azul", "gris" };
 	public char consumos[] = new char[] { 'A', 'B', 'C', 'D', 'E', 'F' };
-
+	// atributos
 	protected double precioBase;
 	protected String color;
 	protected char consumoEnerg;
 	protected double peso;
-
+	// constantes
 	private final String COLOR = colores[0];
 	private final char CONSUMO = consumos[1];
 	private final double PRECIO_BASE = 100;
 	private final double PESO = 5;
 
+	// contructor por defecto
 	public Electrodomestico() {
 		this.precioBase = PRECIO_BASE;
 		this.color = COLOR;
@@ -21,6 +23,8 @@ public class Electrodomestico {
 		this.peso = PESO;
 	}
 
+	// constructor con atributos precioBase y peso pasados por parametro, resto por
+	// defecto
 	public Electrodomestico(double precioBase, double peso) {
 		this.precioBase = precioBase;
 		this.peso = peso;
@@ -28,6 +32,7 @@ public class Electrodomestico {
 		this.consumoEnerg = CONSUMO;
 	}
 
+	// constuctor con todos los atributos
 	public Electrodomestico(double precioBase, String color, char consumoEnerg, double peso) {
 		consumoEnerg = Character.toUpperCase(consumoEnerg);
 		consumoEnerg = comprobarConsumoEnergetico(consumoEnerg);
@@ -39,6 +44,7 @@ public class Electrodomestico {
 		this.peso = peso;
 	}
 
+	// getters y setters
 	public double getPrecioBase() {
 		return precioBase;
 	}
@@ -71,6 +77,12 @@ public class Electrodomestico {
 		this.peso = peso;
 	}
 
+	/***
+	 * 
+	 * @param letra
+	 * @return si la letra correspondiente al consumo energetico no está dentro de
+	 *         las permitidas, el consumo energetico pasa a ser el por defecto
+	 */
 	public char comprobarConsumoEnergetico(char letra) {
 		boolean m_valid = false;
 
@@ -88,6 +100,12 @@ public class Electrodomestico {
 
 	}
 
+	/***
+	 * 
+	 * @param
+	 * @return si el color pasado por parametro no esta dentro de los permitidos, el
+	 *         color pasa a ser el por defecto
+	 */
 	public String comprobarColor(String color) {
 		boolean m_valid = false;
 
@@ -105,33 +123,37 @@ public class Electrodomestico {
 
 	}
 
+	/***
+	 * calcula el precio final del producto en función de su consumo energetico y su
+	 * peso devuelve el precio final
+	 */
 	public void precioFinal() {
 		double precioFinal = this.precioBase;
 
 		switch (this.consumoEnerg) {
-			case 'A':
-				precioFinal += 100;
-				break;
-	
-			case 'B':
-				precioFinal += 80;
-				break;
-	
-			case 'C':
-				precioFinal += 60;
-				break;
-	
-			case 'D':
-				precioFinal += 50;
-				break;
-	
-			case 'E':
-				precioFinal += 30;
-				break;
-	
-			case 'F':
-				precioFinal += 10;
-				break;
+		case 'A':
+			precioFinal += 100;
+			break;
+
+		case 'B':
+			precioFinal += 80;
+			break;
+
+		case 'C':
+			precioFinal += 60;
+			break;
+
+		case 'D':
+			precioFinal += 50;
+			break;
+
+		case 'E':
+			precioFinal += 30;
+			break;
+
+		case 'F':
+			precioFinal += 10;
+			break;
 		}
 
 		if (this.peso >= 0 && this.peso <= 19) {
@@ -147,6 +169,9 @@ public class Electrodomestico {
 		this.precioBase = precioFinal;
 	}
 
+	/***
+	 * sobreescribe el metodo toString() para devolver la información que queremos
+	 */
 	@Override
 	public String toString() {
 		return "Electrodomestico [precioBase=" + precioBase + ", color=" + color + ", consumoEnerg=" + consumoEnerg
