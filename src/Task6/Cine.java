c904ad2d58a3ac0a2a2586e6467bfc107cefac6d
Marccudi <marccudi@hotmail.es>
2022-07-18 09:15:20 +0200

    Class Cine finished

--- src/Task6/Cine.java
+++ src/Task6/Cine.java
@@ -2,4 +2,36 @@ package Task6;
 
 public class Cine {
 
+	private Pelicula pelicula;
+	private double precio;
+	
+	public Cine(Pelicula pelicula, double precio) {
+		
+		this.pelicula = pelicula;
+		this.precio = precio;
+	}
+	public Cine() {
+		this.pelicula = new Pelicula();
+		this.precio = 0;
+	}
+	public Pelicula getPelicula() {
+		return pelicula;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPelicula(Pelicula pelicula) {
+		this.pelicula = pelicula;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cine [pelicula=" + pelicula + ", precio=" + precio + "]";
+	}
+	
+	
+	
 }
