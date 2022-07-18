5a41536c4972fbba83032a5c6fa601352ba7e768
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-18 10:29:30 +0200

    Task6: Add the project

--- src/Task6/Cine.java
+++ src/Task6/Cine.java
@@ -1,9 +1,13 @@
 package Task6;
 
+import java.util.Random;
+
 public class Cine {
 
 	private Pelicula pelicula;
 	private double precio;
+	boolean asientos[] = new boolean[3 * 3];
+	private Integer ocupacion;
 	
 	public Cine(Pelicula pelicula, double precio) {
 		
@@ -20,11 +24,59 @@ public class Cine {
 	public double getPrecio() {
 		return precio;
 	}
+	public Integer getOcupacion() {
+		return ocupacion;
+	}
 	public void setPelicula(Pelicula pelicula) {
 		this.pelicula = pelicula;
 	}
 	public void setPrecio(double precio) {
 		this.precio = precio;
+	}
+	public void setOcupacion(Integer ocupacion) {
+		this.ocupacion = ocupacion;
+	}
+
+	public void añadeEspectador(Espectador espectadores, Cine cine) {
+		Random numeroAleatorio = new Random();
+		Integer numero;
+		boolean m_cond = true;
+		
+		if (this.getOcupacion() < cine.asientos.length)
+		{
+			if (espectadores.getEdad() >= this.getPelicula().getEdadMimina())
+			{
+				if (espectadores.getDinero() >= this.getPrecio())
+				{
+					do {
+						numero = numeroAleatorio.nextInt(9);
+						
+						if (cine.asientos[numero] != true)
+						{
+							m_cond = false;
+						}
+					} while (m_cond);
+					
+					cine.asientos[numero] = true;
+					
+					this.setOcupacion((this.getOcupacion() + 1));
+				}
+				else
+				{
+					System.out.println(espectadores.getNombre()  + " no puede pagar la entrada, tiene " + 
+							espectadores.getDinero() + "€ pero la entrada value " + this.getPrecio() + "€");
+				}
+			}
+			else
+			{
+				System.out.println(espectadores.getNombre() + " no supera la edad mínima de " + this.getPelicula().getEdadMimina()
+						+ "; tiene " + espectadores.getEdad() + " años.");
+			}
+		}
+		else
+		{
+			System.out.println("El cine está lleno, no se puede añadir otro espectador.");
+		}
 	}
 	
 	@Override
--- src/Task6/Task6App.java
+++ src/Task6/Task6App.java
@@ -3,8 +3,38 @@ package Task6;
 public class Task6App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Cine cine = new Cine();
+		
+		Pelicula pelicula = new Pelicula("Anabelle", 90, 18, "pepis");
+		
+		Espectador[] espectadores = {
+					new Espectador("Pepito", 20, 20),
+					new Espectador("Pepita", 30, 10),
+					new Espectador("Juanito", 18, 10),
+					new Espectador("Juanita", 16, 30),
+					new Espectador("Jaimito", 30, 40),
+					new Espectador("Jaimita", 31, 30),
+					new Espectador("Paul", 18, 10),
+					new Espectador("Alice", 16, 30),
+					new Espectador("John Doe", 30, 40),
+					new Espectador("Jane Doe", 31, 30)
+				};
+				
+		for (int i = 0; i < cine.asientos.length; i++) {
+			cine.asientos[i] = false;
+		}
 
+		System.out.println("Espectadores: " + espectadores.length);
+		
+		cine.setOcupacion(0);
+		cine.setPrecio(30);
+		cine.setPelicula(pelicula);
+		
+		for (int i = 0; i < espectadores.length; i++) {
+			cine.añadeEspectador(espectadores[i], cine);
+		}
+		
+		System.out.println("Ocupación: " + cine.getOcupacion() + " Máximo: " + cine.asientos.length);
 	}
 
 }
